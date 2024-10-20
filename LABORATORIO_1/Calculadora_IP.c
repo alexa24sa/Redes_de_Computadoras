e91dab0e8edc151ad38c9a903c6645b158dc3a41
alexa24sa <116330643+alexa24sa@users.noreply.github.com>
2024-10-19 20:04:25 -0600

    Update Calculadora_IP.c
    El siguiente código corrige la parte de la impresión del tipo de la IP haciendo una comparación con XOR junto con la IP de broadcast e IP de Red, posteriormente la comparación byte a byte de la IP una vez que se realiza la XOR. Se quita la parte de la comparación mediante if para el tipo, se coloca la explicación y se añade una nueva función que hace correctamente la conversión de la IP de broadcast negando la máscara de Red

--- LABORATORIO_1/Calculadora_IP.c
+++ LABORATORIO_1/Calculadora_IP.c
@@ -11,10 +11,103 @@ void proceso_anding(unsigned char ip[4], unsigned char mascara_red[4],  unsigned
     //return resultado; // linea de codigo que no es completamente necesaria
 } //funcion para obtener la IP madre o IP red
 
+void proceso_broadcast2(unsigned char ip[4], unsigned char mascara_red[4], unsigned char resultado[4]){
+    
+    //negamos la mascara_red:
+    for(unsigned char i = 0; i<4; i++){
+        mascara_red[i] = ~mascara_red[i];
+    }
+    
+    //aplicamos una xor en todas las posiciones de la IP con la mascara_red
+    for(unsigned char i = 0; i < 4; i++){
+        resultado[i] = ip[i] | mascara_red[i];
+    }
+    
+}
+
+void tipo_de_ip(unsigned char ip[4], unsigned char mascara_red[4]){
+    unsigned char ip_red[4], ip_broadcast[4];
+    
+    //Obtencion de la IP de RED
+    ip_red[0]=ip[0]&mascara_red[0];
+    ip_red[1]=ip[1]&mascara_red[1];
+    ip_red[2]=ip[2]&mascara_red[2];
+    ip_red[3]=ip[3]&mascara_red[3];
+
+    //Obtencion de la IP de Broadcast
+    ip_broadcast[0]=ip[0]|(~mascara_red[0]);
+    ip_broadcast[1]=ip[1]|(~mascara_red[1]);
+    ip_broadcast[2]=ip[2]|(~mascara_red[2]);
+    ip_broadcast[3]=ip[3]|(~mascara_red[3]);
+
+    if((ip[0]^ip_red[0])||(ip[1]^ip_red[1])||(ip[2]^ip_red[2])||(ip[3]^ip_red[3])){
+        /*Supongamos el caso de que nuestra ip fuera la siguiente = 1.0.0.0 
+            entonces aquí lo que se hace es comparar con la máscara de RED, de entrada sabemos que la IP anterior 
+            ese de clase A ya que el primer byte está dentro del rango de 0 a 127, por ende es que es de clase A 
+            Entonces decimos que su máscara de RED es la siguiente: 
+            Máscara de Red = 255.0.0.0 (en la máscara de Red se encienden todos los bits de Red según la clase
+            como sabemos que en las de clase A la correspondencia es R.H.H.H es por eso que toma esos valores
+            ¿Qué pasaría después?, se hará la comparación XOR bit a bit con la IP  con la IP de RED:
+            en las xor para que exista un 1 sabemos que debe haber al menos un 1 encendido, si ambos bits son 1 o 0 (los dos)
+            entonces la salida será cero:
+            Recordatorio: Un byte corresponde a 8 bits que está compuesto por las siguientes potencias:
+            128 64 32 16 8 4 2 1
+            
+            
+            OPERACIÓN:
+                  1.0.0.0                                                00000001.0.0.0
+            XOR 255.0.0.0      RECORDEMOS QUE SON OPERACIONES CON BYTES: 11111111.0.0.0
+            -------------                                               ´--------------
+                254.0.0.0                                                11111110.0.0.0
+            pos: 0  1 2 3 ----- (posiciones de los bits)
+            Posteriormente se hace la OR con cada resultado de cada bit
+            Entonces tenemos que:
+               254            RECORDAR QUE SON OPERACIONES CON BYTES:   11111110
+            OR   0                                                      00000000 
+            OR   0                                                      00000000
+            OR   0                                                      00000000
+            OR   0                                                      00000000
+            OR   0                                                      00000000
+            OR   0                                                      00000000
+            OR   0                                                      00000000    
+            ------                                                      --------
+               255                                                      11111110
+            
+            PERO COMO ESTAMOS TRABAJANDO CON UN IF, VEREMOS QUE NOS VA DAR QUE EL RESULTADO DE TODA ESA OPERACIÓN ES verdadera
+            ENTONCES, en ese caso puede que nuestra IP no sea de RED porque para cualquier valor del primer byte puede dar VERDADERO,
+            (excepto por el caso de la IP 255.0.0.0 que corresponderia a la IP de RED)
+            (254.0.0.0) XOR (255.0.0.0) = 1.0.0.0   Y la OPERACIÓN BIT A BIT ---> (1) OR (0) OR (0) OR (0) = 1 
+            En el caso que todo sea FALSO, ES PORQUE EXACTAMENTE TODOS los bits de inicio, coinciden con la IP de RED
+            POR ENDE en el ELSE de IF se imprime que corresponde a una IP de RED
+            
+            
+            Y es por eso que para la ip de Broadcast no importa si algunos de los bits de RED estan encendidos, nosotros estamos 
+            interesados en saber que los bits de HOST están encendidos, por esa razón es que se coloca el IF DE BROADCAST dentro
+            del IF de RED y de la misma manera, en el caso de que todo sea falso, decimos que se trata de una IP de BROADCAST
+            
+            )
+        
+        */
+        
+        if((ip[0]^ip_broadcast[0])||(ip[1]^ip_broadcast[1])||(ip[2]^ip_broadcast[2])||(ip[3]^ip_broadcast[3])){
+             // esta condicion será verdadera si existe una combinación de bits de RED y HOST encendidos:
+            printf(" Es una IP de tipo HOST.\n");
+        }else{
+            
+            printf(" Es una IP de tipo BROADCAST.\n");
+        }
+        
+        
+    }else{
+        printf(" Es una IP de tipo RED.\n");
+    }
+}
+
+
 void proceso_broadcast(unsigned char clase, unsigned char ip[4], unsigned char resultado[4]){
     //unsigned short int mascara_red[4] = {255, 0, 0, 0};
     // se mandan todos los valores ya que dependiendo de su clase, sus bits de host y red cambian
-    unsigned short int aux = 0;
+    unsigned char aux = 0;
     
     switch (clase){
         case 'A':
@@ -118,7 +211,7 @@ int main(){
         case 'A':
             // ------PARA CONOCER EL TIPO DE MI IP:-------
             printf("El tipo de su IP es: ");
-            if(ip[1]==255){
+            /*if(ip[1]==255){
                 if(ip[2]==255){
                     if(ip[3]==255){
                         printf("Es de tipo broadcast.\n");
@@ -140,7 +233,8 @@ int main(){
                 }else{
                     printf("Es de tipo Host.\n");
                 }
-            }
+            }*/
+            
         
         
         
@@ -152,6 +246,8 @@ int main(){
             mascara_red[1] = 0;
             mascara_red[2] = 0;
             mascara_red[3] = 0;
+            tipo_de_ip(ip, mascara_red);
+            
             proceso_anding(ip, mascara_red, aux_ip);
             
             
@@ -169,7 +265,7 @@ int main(){
             /*for (int i = 0; i < 4; i++){
                 mascara_red[i] = ~mascara_red[i];
             }*/
-            proceso_broadcast(clase, ip, aux_ip);
+            proceso_broadcast2(ip, mascara_red, aux_ip);
             printf("\nSu IP de broadcast es: \n");
             for(unsigned char i =0; i < 4; i++){
                 if (i == 3) {
@@ -185,7 +281,7 @@ int main(){
         case 'B':
             // ------PARA CONOCER EL TIPO DE MI IP:-------
             printf("El tipo de su IP es: ");
-            if(ip[2]==255){
+            /*if(ip[2]==255){
                 if(ip[3]==255){
                     printf("Es de tipo broadcast.\n");
                 }else{
@@ -201,7 +297,7 @@ int main(){
                 }else{
                     printf("Es de tipo Host.\n");
                 }
-            }
+            }*/
         
         
         
@@ -212,6 +308,8 @@ int main(){
             mascara_red[1] = 255;
             mascara_red[2] = 0;
             mascara_red[3] = 0;
+            tipo_de_ip(ip, mascara_red);
+            
             proceso_anding(ip, mascara_red, aux_ip);
             printf("Su IP madre o IP de Red es: \n");
             for(unsigned char i=0; i < 4; i++){
@@ -227,7 +325,8 @@ int main(){
             /*for (int i = 0; i < 4; i++){
                 mascara_red[i] = ~mascara_red[i];
             }*/
-            proceso_broadcast(clase, ip, aux_ip);
+            
+            proceso_broadcast2(ip, mascara_red, aux_ip);
             printf("\nSu IP de broadcast es: \n");
             for(unsigned char i=0; i < 4; i++){
                 if (i == 3) {
@@ -244,7 +343,7 @@ int main(){
         case 'C':
             // ------PARA CONOCER EL TIPO DE MI IP:-------
             printf("El tipo de su IP es: ");
-            if(ip[3]==255){
+            /*if(ip[3]==255){
                 printf("Es de tipo broadcast.\n");
             }else{
                 if(ip[3]==0){
@@ -252,7 +351,7 @@ int main(){
                 }else{
                     printf("Es de tipo Host.\n");
                 }
-            }
+            }*/
         
         
             //Para obtener su IP de red:
@@ -261,6 +360,8 @@ int main(){
             mascara_red[1] = 255;
             mascara_red[2] = 255;
             mascara_red[3] = 0;
+            tipo_de_ip(ip, mascara_red);
+            
             proceso_anding(ip, mascara_red, aux_ip);
             printf("Su IP madre o IP de Red es: \n");
             for(unsigned char i =0; i < 4; i++){
@@ -276,7 +377,7 @@ int main(){
             /*for (int i = 0; i < 4; i++){
                 mascara_red[i] = ~mascara_red[i];
             }*/
-            proceso_broadcast(clase, ip, aux_ip);
+            proceso_broadcast2(ip, mascara_red, aux_ip);
             printf("\nSu IP de broadcast es: \n");
             for(unsigned char i =0; i < 4; i++){
                 if (i == 3) {
